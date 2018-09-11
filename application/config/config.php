c0c46924ce0d020b34e6889d84b79f5caa3b8eda
Aluno <Aluno@DESKTOP-6PK3RGQ>
2018-09-10 21:41:08 -0300

    base url

--- application/config/config.php
+++ application/config/config.php
@@ -26,6 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //$config['base_url'] = 'http://localhost/Projeto';
 $config['base_url'] = 'http://projetoxxx-com.umbler.net';
 
+
 /*
 |--------------------------------------------------------------------------
 | Index File
