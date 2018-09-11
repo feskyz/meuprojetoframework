ea422083fc19410cc46e9d44b00c1c5d8603d581
Aluno <Aluno@DESKTOP-6PK3RGQ>
2018-09-10 21:45:44 -0300

    base url

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,11 +5,11 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-<head>
-<title>Meu Projeto Framework</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <title>Meu Projeto Framework</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/bootstrap-grid.min.css') ?>" rel="stylesheet">
-</head>
-<body>
+        <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+        <link href="<?php echo base_url('assets/css/bootstrap-grid.min.css') ?>" rel="stylesheet">
+    </head>
+    <body>
